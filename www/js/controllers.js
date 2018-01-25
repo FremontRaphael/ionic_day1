a643d780c42fc909b8d7afaf6a7794c21364aefd
Lowbringer <gaetan.fritz@epitech.eu>
2018-01-25 17:07:30 +0100

    update

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', function($scope, Courses) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -11,14 +11,12 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+  $scope.Courses = Courses.all();
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatDetailCtrl', function($scope, $stateParams, Courses) {
+  $scope.course = Courses.get($stateParams.chatId);
+  console.log("course = " + $scope.course);
 })
 
 .controller('AccountCtrl', function($scope) {
